cb296438ace81628f60b5b517399c4be83640b8c
Scott Bowler <scott.bowler@learningfuze.com>
2018-02-06 12:36:50 -0800

    Initial app set up with dummy data

---
+++ src/assets/data/index.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        title: 'First todo item',
+        complete: false
+    },
+    {
+        title: 'Make to do app',
+        complete: false
+    },
+    {
+        title: 'Wash the car',
+        complete: false
+    },
+    {
+        title: 'Go to store',
+        complete: false
+    }
+];
